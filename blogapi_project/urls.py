20a52e471fc517266ab5fca5b00a0d7772a5f5ca
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-09-07 01:26:33 +0300

    almost done

--- blogapi_project/urls.py
+++ blogapi_project/urls.py
@@ -1,4 +1,4 @@
-"""drf_api_permissions_postgres URL Configuration
+"""blogapi_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/snacks/', include('snacks.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
